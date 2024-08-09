6066979944940ca8a0d5290c3eb59347bc3c8b17
Anjali <anjali.dubey@comprotechnologies.com>
2024-08-09 16:56:06 +0530

    Issues : Resolved all the issues mentioned.

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 let books = [];
 
+// Function to fetch data from books.json
 async function fetchBooksData() {
-  const response = await fetch("books.json");
-  const result = await response.json();
-  books = result;
+  const response = await fetch('books.json');
+  books = await response.json();
   displayData(books);
 }
-
 fetchBooksData();
 
+/*
+Function to search a book by its unique id
+@param {number} bookId - The id of the book
+*/
 function searchBookById(bookId) {
   return new Promise((resolve) => {
-    const book = books.find((b) => b.bookId == bookId);
-    resolve(book);
+    resolve(books.find((b) => b.bookId === bookId));
   });
 }
 
+/*
+Function to search books by its genre
+@param {string} genre - The genre of the book
+*/
 function searchBooksByGenre(genre) {
   return new Promise((resolve) => {
     const filteredBooks = books.filter(
@@ -25,6 +31,10 @@ function searchBooksByGenre(genre) {
   });
 }
 
+/*
+Function to search books by its price
+@param {number} price - The price of the book
+*/
 function searchBooksByPrice(price) {
   return new Promise((resolve) => {
     const filteredBooks = books.filter((b) => b.price === price);
@@ -32,85 +42,83 @@ function searchBooksByPrice(price) {
   });
 }
 
+/*
+Function to sort books in ascending/descending order of price
+@param {string} order - The order(asc/desc) of the book's price
+*/
 function sortBooksByPrice(order) {
   return new Promise((resolve) => {
     books.sort((a, b) =>
-      order === "asc" ? a.price - b.price : b.price - a.price
+      order === 'asc' ? a.price - b.price : b.price - a.price
     );
     resolve(books);
   });
 }
 
-document
-  .getElementById("arrow-up-asc")
-  .addEventListener("click", async function () {
-    const sortedBooks = await sortBooksByPrice("asc");
-    displayData(sortedBooks);
-  });
+document.getElementById('arrow-up-asc').addEventListener('click', async function () {
+  const sortedBooks = await sortBooksByPrice('asc');
+  displayData(sortedBooks);
+});
 
-document
-  .getElementById("arrow-down-desc")
-  .addEventListener("click", async function () {
-    const sortedBooks = await sortBooksByPrice("desc");
-    displayData(sortedBooks);
-  });
+document.getElementById('arrow-down-desc').addEventListener('click', async function () {
+  const sortedBooks = await sortBooksByPrice('desc');
+  displayData(sortedBooks);
+});
 
-document
-  .getElementById("btn-search-id")
-  .addEventListener("click", async function (event) {
-    event.preventDefault();
-    const inputElement = document.getElementById("input-search-id");
-    const bookId = inputElement.value;
-    if (bookId) {
-      const book = await searchBookById(bookId);
-      if (book) {
-        displaySimilarData([book]);
-      } else {
-        console.log("Book not found");
-      }
+document.getElementById('btn-search-id').addEventListener('click', async function (event) {
+  event.preventDefault();
+  const inputElement = document.getElementById('input-search-id');
+  const bookId = inputElement.value;
+  if (bookId) {
+    const book = await searchBookById(Number(bookId));
+    if (book) {
+      displaySimilarData([book]);
+    } else {
+      console.log('Book not found');
     }
-    inputElement.value = "";
-  });
+  }
+  inputElement.value = '';
+});
 
-document
-  .getElementById("btn-search-genre")
-  .addEventListener("click", async function (event) {
-    event.preventDefault();
-    const inputElement = document.getElementById("input-search-genre");
-    const genre = inputElement.value;
-    if (genre) {
-      const books = await searchBooksByGenre(genre);
-      if (books) {
-        displaySimilarData(books);
-      } else {
-        console.log("Genre not found");
-      }
+document.getElementById('btn-search-genre').addEventListener('click', async function (event) {
+  event.preventDefault();
+  const inputElement = document.getElementById('input-search-genre');
+  const genre = inputElement.value;
+  if (genre) {
+    const books = await searchBooksByGenre(genre);
+    if (books) {
+      displaySimilarData(books);
+    } else {
+      console.log('Genre not found');
     }
-    inputElement.value = "";
-  });
+  }
+  inputElement.value = '';
+});
 
-document
-  .getElementById("btn-search-price")
-  .addEventListener("click", async function (event) {
-    event.preventDefault();
-    const inputElement = document.getElementById("input-search-price");
-    const price = inputElement.value;
-    if (price) {
-      const books = await searchBooksByPrice(Number(price));
-      if (books) {
-        displaySimilarData(books);
-      } else {
-        console.log("price not found");
-      }
+document.getElementById('btn-search-price').addEventListener('click', async function (event) {
+  event.preventDefault();
+  const inputElement = document.getElementById('input-search-price');
+  const price = inputElement.value;
+  if (price) {
+    const books = await searchBooksByPrice(Number(price));
+    if (books) {
+      displaySimilarData(books);
+    } else {
+      console.log('price not found');
     }
-    inputElement.value = "";
-  });
+  }
+  inputElement.value = '';
+});
 
+/*
+Function to display similar books when searched with id, genre, and price
+@param {array} books - The list of all similar books
+*/
 function displaySimilarData(books) {
-  const similarBooks = document.querySelector("#table-similar-books tbody");
-  similarBooks.innerHTML = "";
+  const similarBooks = document.querySelector('#table-similar-books tbody');
+  similarBooks.innerHTML = '';
   books.forEach((book) => {
-    const tr = document.createElement("tr");
+    const tr = document.createElement('tr');
     tr.innerHTML = `
       <td>${book.bookId}</td>
       <td>${book.genre}</td>
@@ -121,11 +129,15 @@ function displaySimilarData(books) {
   });
 }
 
+/*
+Function to display all the books from json file
+@param {array} books - The list of all books
+*/
 function displayData(books) {
-  const allBooks = document.querySelector("#table-all-books tbody");
-  allBooks.innerHTML = "";
+  const allBooks = document.querySelector('#table-all-books tbody');
+  allBooks.innerHTML = '';
   books.forEach((book) => {
-    const tr = document.createElement("tr");
+    const tr = document.createElement('tr');
     tr.innerHTML = `
       <td>${book.bookId}</td>
       <td>${book.genre}</td>
@@ -136,41 +148,49 @@ function displayData(books) {
   });
 }
 
+/*
+Function to display current examined book 
+@param {object} book - The current examined book
+*/
 function displayExaminedBook(book) {
-  const bookIdSpan = document.querySelector("#examined-book-id");
-  const priceSpan = document.querySelector("#examined-book-price");
-  const genreSpan = document.querySelector("#examined-book-genre");
+  const bookIdSpan = document.getElementById('examined-book-id');
+  const priceSpan = document.getElementById('examined-book-price');
+  const genreSpan = document.getElementById('examined-book-genre');
   bookIdSpan.textContent = book.bookId;
   genreSpan.textContent = book.genre;
   priceSpan.textContent = book.price;
 }
 
+/*
+Function to examine books by clicking on examine button
+@param {number} bookId - The id of the book being examined
+*/
 function examineBook(bookId) {
   const book = books.find((b) => b.bookId === bookId);
   if (book) {
-    const modalBody = document.getElementById("modal-body");
+    const modalBody = document.getElementById('modal-body');
     modalBody.innerHTML = `
       <h2>Book Details</h2>
       <p>ID: ${book.bookId}</p>
       <p>Genre: ${book.genre}</p>
       <p>Price: ${book.price}</p>
     `;
-    const modal = document.getElementById("bookModal");
-    modal.style.display = "block";
+    const modal = document.getElementById('bookModal');
+    modal.style.display = 'block';
 
-    const closeButton = document.querySelector(".close");
+    const closeButton = document.querySelector('.js-close');
     closeButton.onclick = function () {
-      modal.style.display = "none";
+      modal.style.display = 'none';
     };
 
     window.onkeydown = function (e) {
-      if (e.key === "Escape") {
-        modal.style.display = "none";
+      if (e.key === 'Escape') {
+        modal.style.display = 'none';
       }
     };
 
     displayExaminedBook(book);
   } else {
-    console.log("Book not found");
+    console.log('Book not found');
   }
 }
